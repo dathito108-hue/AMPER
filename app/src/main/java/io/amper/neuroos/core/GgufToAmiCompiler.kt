0ecd9b32c0bd47243d1ceac45524379701145036
dathito108-hue <dathito108@gmail.com>
2026-09-20 22:16:01 +0700

    Phase628: keep legacy compiler stream helpers compiling

--- app/src/main/java/io/amper/neuroos/core/GgufToAmiCompiler.kt
+++ app/src/main/java/io/amper/neuroos/core/GgufToAmiCompiler.kt
@@ -4,6 +4,7 @@ import java.io.EOFException
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.io.InputStream
 import java.io.RandomAccessFile
 import java.security.MessageDigest
 
